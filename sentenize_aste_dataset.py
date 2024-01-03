e45493f191d86531c7737b9a00157f2a14d8e610
Lev Morozov <lamorozov@sberbank.ru>
2024-01-03 14:37:16 +0300

    only keep sents with opinions inplace

--- sentenize_aste_dataset.py
+++ sentenize_aste_dataset.py
@@ -21,6 +21,7 @@ def main(src, dst):
             triplets = eval(triplets)
             n_triplets += len(triplets)
             triplets_aspects = [min(triplet[0]) for triplet in triplets]
+            triplets_opinions = [min(triplet[1]) for triplet in triplets]
             line_entries = []
             offset = 0
             sents = [ el.text for el in list(sentenize(text)) ]
@@ -31,8 +32,11 @@ def main(src, dst):
                 sent_tokens = sent.split(" ")
                 sent_aspects = [
                     i 
-                    for i, aspect_start_token in enumerate(triplets_aspects) 
-                    if offset <= aspect_start_token < len(sent_tokens) + offset 
+                    for i, (aspect_start_token, opinion_start_token) in enumerate(zip(triplets_aspects, triplets_opinions)) 
+                    if (
+                        offset <= aspect_start_token < len(sent_tokens) + offset and
+                        offset <= opinion_start_token < len(sent_tokens) + offset 
+                    )
                 ]
                 if len(sent_aspects) != 0:
                     sent_triplets = [
@@ -46,10 +50,13 @@ def main(src, dst):
                                 assert triplet[j][i] < len(sent_tokens), (triplet[j][i], len(sent_tokens))
                     line_entries.append((sent, sent_triplets))
                 offset += len(sent_tokens)
-            assert sum(len(line_entry[1]) for line_entry in line_entries) == len(triplets)
             entries.append(line_entries)
 
-        assert sum(len(entry[1]) for entry_set in entries for entry in entry_set) == n_triplets
+        n_new_triplets = sum(len(entry[1]) for entry_set in entries for entry in entry_set)
+        print(
+            f"{n_triplets - n_new_triplets} ({round(1 - n_new_triplets/n_triplets, 2) * 100}%) " +
+            f"entries in {file} contained outplace opinions and were therefore removed"
+        )
 
         entries_pooled = [
             entry
